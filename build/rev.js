30c2b34b1db989f5627c51ba5590c28a481bdbc4
yujingwyh <yujingwyh@163.com>
2017-05-17 09:38:41 +0800

    使用 gulp-rev-handle 处理版本控制

--- build/rev.js
+++ build/rev.js
@@ -1,8 +1,15 @@
-var revReplace = require('./rev-replace.js');
+var revHandle = require('gulp-rev-handle');
 
 module.exports = function (gulp, config) {
   /*版本替换*/
   return gulp.src(config.rev.input)
-    .pipe(revReplace(config.output, config.rev.realityPath))
+    .pipe(revHandle({
+      handlePath(path){
+        return config.output + path;
+      },
+      handleRev(path, md5){
+        return path.replace(config.output, './') + '?version=' + md5;
+      }
+    }))
     .pipe(gulp.dest(config.output));
 };
